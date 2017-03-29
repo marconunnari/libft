38a4963746048ccabbf238bdf0bc8c90b98cc2b5
Marco Nunnari <marnunna@gmail.com>
2017-03-29 17:32:05 +0200

    do ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,1 +1,18 @@
-//void			*ft_memcpy(void *dest, const void *src, size_t n);
+#include "libft.h"
+
+void			*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int	i;
+	char		*source;
+	char		*destination;
+
+	i = 0;
+	source = (char*) src;
+	destination = (char*) dest;
+	while(source[i] && i < n)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	return (dest);
+}
