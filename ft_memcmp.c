ed98e4e2e1dc2562f8f7bff6be58c4315f1f6dc9
Marco Nunnari <marnunna@gmail.com>
2017-03-31 00:51:45 +0200

    little fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -8,6 +8,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	int		res;
 
 	i = 0;
+	res = 0;
 	str1 = (unsigned char*) s1;
 	str2 = (unsigned char*) s2;
 	while (i < n && !(res = str1[i] - str2[i]))
