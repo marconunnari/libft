4d107b3f835627c0e1f603d2eafdc7f7b77a41d8
Marco Nunnari <marnunna@gmail.com>
2017-03-30 23:00:21 +0200

    do ft_memmove

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ SRCS =	ft_memset.c \
 	ft_bzero.c \
 	ft_memcpy.c \
 	ft_memccpy.c \
+	ft_memmove.c \
 	#new#
 
 OBJS = $(SRCS:.c=.o)
---
+++ ft_memmove.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+void		*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*destination;
+	unsigned char	*source;
+	unsigned char	*buffer;
+	unsigned int	i;
+
+	if (dest == NULL || src == NULL)
+		return (dest);
+	destination = (unsigned char*) dest;
+	source = (unsigned char*) src;
+	buffer = (unsigned char*) malloc(n);
+	i = 0;
+	while(i < n)
+	{
+		buffer[i] = source[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		destination[i] = buffer[i];
+		i++;
+	}
+	return (dest);
+}
