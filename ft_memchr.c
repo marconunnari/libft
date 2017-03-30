60e673d4c623c02d4a08e12216f21efaa4cef987
Marco Nunnari <marnunna@gmail.com>
2017-03-31 00:11:26 +0200

    do ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char		*str;
+	unsigned char		chr;
+	unsigned int			i;
+
+	if (s == NULL)
+		return (NULL);
+	str = (unsigned char *) s;
+	chr = (unsigned char) c;
+	i = 0;
+	while (i < n && str[i] != chr)
+		i++;
+	if (i != n)
+		return (&str[i]);
+	else
+		return (NULL);
+}
