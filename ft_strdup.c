07a1e00a6129bee4dbeacfb1601da75aaff431ee
Marco Nunnari <marnunna@gmail.com>
2017-03-31 19:51:54 +0200

    malloc failure

--- ft_strdup.c
+++ ft_strdup.c
@@ -7,6 +7,9 @@ char		*ft_strdup(const char *s)
 
 	len = ft_strlen(s);
 	dup = (char*) malloc(len + 1);
-	ft_memcpy(dup, s, len + 1);
+	if (!dup)
+		return (NULL);
+	else
+		ft_memcpy(dup, s, len + 1);
 	return (dup);
 }
