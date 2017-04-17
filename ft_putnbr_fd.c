ce982f74b3cf880521b72726e9e7a5744d4c0014
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-17 20:05:23 +0200

    fixes

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,33 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 18:59:26 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/04/17 19:15:06 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/04/17 19:59:52 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void		mn_putnbr_fd(int nb, int fd)
+{
+	if (nb > 0)
+		nb = nb * -1;
+	if (nb > -10)
+	{
+		nb *= -1;
+		ft_putchar_fd(nb + '0', fd);
+	}
+	else
+	{
+		mn_putnbr_fd(nb / 10, fd);
+		mn_putnbr_fd(nb % 10, fd);
+	}
+}
+
 void			ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+	}
+	mn_putnbr_fd(n, fd);
 }
--- ft_putstr.c
+++ ft_putstr.c
@@ -6,18 +6,13 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 18:59:27 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/04/17 19:13:54 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/04/17 20:01:51 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void		printchar(char *c)
-{
-	ft_putchar(*c);
-}
-
 void			ft_putstr(char const *s)
 {
-	ft_striter((char*)s, &printchar);
+	ft_putstr_fd(s, 1);
 }
