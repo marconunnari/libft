f6ed4c281fc903b4c38681116a375d78f5c41cc1
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-18 17:29:36 +0200

    norminated

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/18 17:14:21 by mnunnari          #+#    #+#             */
+/*   Updated: 2017/04/18 17:15:02 by mnunnari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
@@ -7,7 +19,7 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 
 	node = f(lst);
 	res = node;
-	while(lst->next)
+	while (lst->next)
 	{
 		lst = lst->next;
 		node->next = f(lst);
