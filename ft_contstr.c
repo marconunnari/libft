53d1ad8dcbee6a23c186e9f45572b500db572555
Marco Nunnari <mnunnari@student.42.us.org>
2017-05-11 21:12:59 +0200

    fixed ft_contstr

--- ft_contstr.c
+++ ft_contstr.c
@@ -6,7 +6,7 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 16:18:15 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/04/27 16:18:16 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/05/11 21:12:48 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,12 @@
 char		*ft_contstr(char **arr, const char *s)
 {
 	int		i;
-	char	*strsub;
 
 	i = 0;
 	while (arr[i])
 	{
-		strsub = ft_strsub(s, 0, ft_strlen(arr[i]));
-		if (ft_strequ(arr[i], strsub))
-		{
-			free(strsub);
+		if (ft_strequ(arr[i], s))
 			return (arr[i]);
-		}
-		free(strsub);
 		i++;
 	}
 	return (NULL);
