cc92a949e968db51477c036118b313697cccd056
Marco Nunnari <mnunnari@student.42.us.org>
2017-05-27 22:27:00 +0200

    nl

--- srcs/printf/ft_error.c
+++ srcs/printf/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/27 22:19:31 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/05/27 22:22:19 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/05/27 22:26:34 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void		ft_error(char *msg, int exit_code)
 {
-	ft_dprintf(2, msg);
+	ft_dprintf(2, "%s\n", msg);
 	exit(exit_code);
 }
