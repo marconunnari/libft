21b3a8c1b00158b34e76d29bdf5e4891f6d2ff22
Marco Nunnari <mnunnari@student.42.us.org>
2017-06-26 22:56:25 +0200

    ok

--- srcs/str/ft_strchrs.c
+++ srcs/str/ft_strchrs.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchrs.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/26 21:09:17 by mnunnari          #+#    #+#             */
+/*   Updated: 2017/06/26 21:09:36 by mnunnari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char		*ft_strchrs(const char *s, int (*f)(char))
@@ -11,5 +23,7 @@ char		*ft_strchrs(const char *s, int (*f)(char))
 			return ((char *)(s + i));
 		i++;
 	}
+	if (f(s[i]))
+		return ((char *)(s + i));
 	return (NULL);
 }
